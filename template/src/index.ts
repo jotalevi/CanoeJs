e3be150d03a399455b6caf7fb33c2c12d8b78b16
unknown <talevineto@gmail.com>
2025-03-19 00:29:34 -0300

    Changes to template

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,5 +1,6 @@
-import { Canoe, Col, H, Row, Link, FlexAlignContent, FlexAlignItems, FlexJustify } from "canoejs";
-import Logo from "./widgets/Logo";
+import { Canoe, Router } from "canoejs";
+import HomePage from "./pages/HomePage";
+import DocsPage from "./pages/DocsPage";
 
 // This runs before every render cycle, here you should only add elements to the head of the document, the document itself and window.
 Canoe.preBuild(() => {
@@ -14,104 +15,17 @@ Canoe.postBuild(() => {
 })
 
 Canoe.buildApp("root", {}, (state) => {
-  return new Col({
-    alignContent: FlexAlignContent.CENTER,
-    justify: FlexJustify.CENTER,
-    alignItems: FlexAlignItems.CENTER,
-    style: {
-      width: "100%",
-      height: "100%",
-    },
-    gap: "0px",
-    children: [
-      new Logo(),
-      new H({
-        size: 1,
-        text: "CanoeJS",
-        style: {
-          margin: "0px",
-          color: "#000",
-          fontSize: "50px",
-          textAlign: "center",
-        },
-      }),
-      new H({
-        size: 2,
-        text: "A JavaScript framework for building web applications",
-        style: {
-          margin: "0px",
-          color: "#000",
-          fontSize: "20px",
-          textAlign: "center",
-        },
-      }),
-      new Row({
-        alignContent: FlexAlignContent.CENTER,
-        justify: FlexJustify.CENTER,
-        alignItems: FlexAlignItems.CENTER,
-        children: [
-          new Link({
-            to: "https://www.github.com/jotalevi/CanoeJs",
-            children: [
-              new H({
-                size: 3,
-                text: "GitHub",
-                style: {
-                  color: "rgb(148 190 200)",
-                  fontSize: "20px",
-                  textAlign: "center",
-                },
-              }),
-            ]
-          }),
-          new H({
-            size: 2,
-            text: "|",
-            style: {
-              color: "#000",
-              fontSize: "20px",
-              textAlign: "center",
-            },
-          }),
-          new Link({
-            to: "https://github.com/jotalevi/CanoeJs",
-            children: [
-              new H({
-                size: 3,
-                text: "Documentation",
-                style: {
-                  color: "rgb(148 190 200)",
-                  fontSize: "20px",
-                  textAlign: "center",
-                },
-              }),
-            ]
-          }),
-          new H({
-            size: 2,
-            text: "|",
-            style: {
-              color: "#000",
-              fontSize: "20px",
-              textAlign: "center",
-            },
-          }),
-          new Link({
-            to: "https://github.com/jotalevi/CanoeJs",
-            children: [
-              new H({
-                size: 3,
-                text: "Examples",
-                style: {
-                  color: "rgb(148 190 200)",
-                  fontSize: "20px",
-                  textAlign: "center",
-                },
-              }),
-            ]
-          }),
-        ]
-      })
-    ]
+  return new Router({
+    routes: [
+      {
+        route: '/',
+        widget: new HomePage({})
+      },
+      {
+        route: '/docs',
+        widget: new DocsPage()
+      }
+    ],
+    currentRoute: state.url ?? '/',
   });
 }).render();
---
+++ template/src/pages/DocsPage.ts
@@ -0,0 +1,18 @@
+import { Widget } from "canoejs";
+
+export default class DocsPage implements Widget {
+    id: string
+
+    constructor() {
+        this.id = "docs-page";
+    }
+
+    render(): HTMLElement {
+        const container = document.createElement("div");
+        container.innerHTML = `
+            <h1>Documentation</h1>
+            <p>Here you will find the documentation for CanoeJS</p>
+        `;
+        return container;
+    }
+}
