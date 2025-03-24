1c70a34f0ced766f71d733135ac05d76297130cc
jotalevi <talevineto@gmail.com>
2025-03-24 12:37:47 -0300

    Change CLI (Added -v), Added text elements, version bump

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,4 +1,6 @@
-import { Canoe, Router } from "canoejs";
+import { Canoe, Col, FlexAlignContent, FlexAlignItems, FlexJustify, FlexWrap, Row } from "canoejs";
+import Logo from "./widgets/Logo";
+import TempText from "./widgets/TempText";
 
 // This runs before every render cycle, here you should only add elements to the head of the document, the document itself and window.
 Canoe.preBuild(() => {
@@ -15,5 +17,98 @@ Canoe.postBuild(() => {
 })
 
 Canoe.buildApp("root", { url: '/' }, (state) => {
-
+  return new Col({
+    flexAlignContent: FlexAlignContent.CENTER,
+    flexAlignItems: FlexAlignItems.CENTER,
+    flexJustify: FlexJustify.CENTER,
+    flexWrap: FlexWrap.NOWRAP,
+    children: [
+      new Logo({}),
+      new TempText({
+        id: "title",
+        text: "CanoeJS",
+        css: {
+          fontSize: "20px",
+          color: "black",
+          marginTop: "20px",
+          fontFamily: "Arial, sans-serif",
+          textAlign: "center",
+        }
+      }),
+      new TempText({
+        id: "title",
+        text: "A lightweight framework for building web applications",
+        css: {
+          fontSize: "16px",
+          color: "black",
+          marginTop: "10px",
+          fontFamily: "Arial, sans-serif",
+          textAlign: "center",
+        }
+      }),
+      new Row({
+        flexAlignContent: FlexAlignContent.CENTER,
+        flexAlignItems: FlexAlignItems.CENTER,
+        flexJustify: FlexJustify.CENTER,
+        flexWrap: FlexWrap.NOWRAP,
+        children: [
+          new TempText({
+            id: "l1",
+            text: "GitHub",
+            css: {
+              fontSize: "16px",
+              color: "black",
+              marginTop: "10px",
+              fontFamily: "Arial, sans-serif",
+              textAlign: "center",
+            }
+          }),
+          new TempText({
+            id: "s1",
+            text: "|",
+            css: {
+              fontSize: "16px",
+              color: "black",
+              marginTop: "10px",
+              fontFamily: "Arial, sans-serif",
+              textAlign: "center",
+            }
+          }),
+          new TempText({
+            id: "l2",
+            text: "Documentation",
+            css: {
+              fontSize: "16px",
+              color: "black",
+              marginTop: "10px",
+              fontFamily: "Arial, sans-serif",
+              textAlign: "center",
+            }
+          }),
+          new TempText({
+            id: "s2",
+            text: "|",
+            css: {
+              fontSize: "16px",
+              color: "black",
+              marginTop: "10px",
+              fontFamily: "Arial, sans-serif",
+              textAlign: "center",
+            }
+          }),
+          new TempText({
+            id: "l3",
+            text: "Examples",
+            css: {
+              fontSize: "16px",
+              color: "black",
+              marginTop: "10px",
+              fontFamily: "Arial, sans-serif",
+              textAlign: "center",
+            }
+          })
+        ]
+      })
+    ]
+  })
 }).render();
